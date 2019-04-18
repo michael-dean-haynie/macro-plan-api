29df316e5b3625b208eb60c8543c4a0f6959e628
Michael Dean Haynie <michael.dean.haynie@gmail.com>
2019-04-18 01:56:17 -0400

    Change callSingle to call

--- src/test/java/karate-config.js
+++ src/test/java/karate-config.js
@@ -12,7 +12,15 @@ function fn(){
 	  karate.configure('readTimeout', 5000);
 	  
 	  // prepare schemas
-	  var result = karate.callSingle('classpath:schemas.feature', config);
+	  // 
+	  // at first callSingle() seemed more appropriate but after a LOT of debugging I finally
+	  // figured out that it doesn't quite work how you would think. It is guaranteed to only run once, but 
+	  // in a race-condition seeming situation it will not always complete before the tests start running. 
+	  // This resulted in add schema validation errors where the double nested schema seemed to have not been parsed yet and 
+	  // was participating as a string literal instead of the actual schema ???
+	  //
+	  // ex: com.intuit.karate.exception.KarateException: food.feature:161 - path: $[0].unit, actual: {id=1, unitSystem=METRIC, unitType=MASS, unit=MILLIGRAM, properName=Milligram, abbreviation=mg, unitTypeRatio=0.001}, expected: '#java.util.LinkedHashMap', reason: not equal
+	  var result = karate.call('classpath:schemas.feature', config);
 	  config.schemas = result.schemas;
 	  
 	  return config;
