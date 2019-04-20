dd079b113d985ef80834e3ef9630c73c52d62035
Michael Dean Haynie <michael.dean.haynie@gmail.com>
2019-04-20 15:32:42 -0400

    Rename Food -> FoodTemplate, same with FoodRepository

---
+++ src/main/java/com/codetudes/macroplanapi/db/domain/FoodTemplate.java
@@ -0,0 +1,55 @@
+package com.codetudes.macroplanapi.db.domain;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import org.hibernate.annotations.Formula;
+
+import lombok.Data;
+
+@Data
+@Entity(name="food")
+public class FoodTemplate {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(columnDefinition = "serial")
+	private Long id;
+	
+	private Integer calories;
+	
+	private Double fat;
+	
+	private Double carbs;
+	
+	private Double protein;
+	
+	private String name;
+	
+	private String brand;
+	
+	private String styleOrFlavor;
+	
+	@OneToMany(cascade= CascadeType.ALL, orphanRemoval = true)
+	private List<Measurement> measurements;
+	
+	private Boolean isTemplate;
+	
+	/**
+	 * Calculated fields (not returned, just used for sorting)
+	 */
+	@Formula("(fat * 9) / ((fat * 9) + (carbs * 4) + (protein * 4)) * 100")
+	private Double fatPercentage;
+	
+	@Formula("(carbs * 4) / ((fat * 9) + (carbs * 4) + (protein * 4)) * 100")
+	private Double carbsPercentage;
+	
+	@Formula("(protein * 4) / ((fat * 9) + (carbs * 4) + (protein * 4)) * 100")
+	private Double proteinPercentage;
+}
---
+++ src/main/java/com/codetudes/macroplanapi/db/repository/FoodTemplateRepository.java
@@ -0,0 +1,25 @@
+package com.codetudes.macroplanapi.db.repository;
+
+import java.util.List;
+
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.codetudes.macroplanapi.db.domain.FoodTemplate;
+
+@Repository
+public interface FoodTemplateRepository extends PagingAndSortingRepository<FoodTemplate, Long> {
+	@Query(
+		"SELECT f FROM com.codetudes.macroplanapi.db.domain.FoodTemplate f WHERE " +
+		"(f.isTemplate = true) AND " +
+		"(" +
+		    "LOWER(f.name) LIKE LOWER(CONCAT('%',:searchTerm, '%')) OR " +
+		    "LOWER(f.brand) LIKE LOWER(CONCAT('%',:searchTerm, '%')) OR " +
+		    "LOWER(f.styleOrFlavor) LIKE LOWER(CONCAT('%',:searchTerm, '%'))" +
+	    ")"
+    )
+    List<FoodTemplate> findAllTemplatesWithSearchAndSort(@Param("searchTerm") String searchTerm, Sort sort);
+}
